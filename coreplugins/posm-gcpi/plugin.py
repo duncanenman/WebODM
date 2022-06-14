fe683e7ebbb073ec65570d822bda2ff9e762e17c
Duncan Enman <denman@thewebpeople.ca>
2022-06-14 10:17:37 -0300

    Update plugin.py

--- coreplugins/posm-gcpi/plugin.py
+++ coreplugins/posm-gcpi/plugin.py
@@ -4,8 +4,8 @@ from django.utils.translation import gettext as _
 
 class Plugin(PluginBase):
 
-    def main_menu(self):
-        return [Menu(_("GCP Interface"), self.public_url(""), "fa fa-map-marker-alt fa-fw")]
+    '''def main_menu(self):
+        return [Menu(_("GCP Interface"), self.public_url(""), "fa fa-map-marker-alt fa-fw")]'''
 
     def app_mount_points(self):
         return [
